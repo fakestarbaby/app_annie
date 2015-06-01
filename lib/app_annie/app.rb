2f1e44811a16f25d11cc061d408ae6e5d0926cde
Pavel Borsky <pavelb@zeptolab.com>
2015-06-01 12:06:30 +0300

    App features & ranks api

--- lib/app_annie/app.rb
+++ lib/app_annie/app.rb
@@ -35,13 +35,22 @@ module AppAnnie
       request "/v1.2/accounts/#{account_id}/products/#{product_id}/sales", params
     end
 
+    def features(params = nil)
+      raise MissingArgument, 'Param :start_date is required' unless params.has_key? :start_date
+      raise MissingArgument, 'Param :end_date is required' unless params.has_key? :end_date
+
+      request "/v1.2/#{vertical}/#{market}/#{asset}/#{product_id}/features", params
+    end
+
     def ranks(params = nil)
       raise MissingArgument, 'Param :start_date is required' unless params.has_key? :start_date
       raise MissingArgument, 'Param :end_date is required' unless params.has_key? :end_date
 
-      device = params[:device]
-      device = @product['devices'].first if device.nil? and !@product['devices'].nil?
-      raise MissingArgument, 'Param :device is required' if device.nil?
+      unless params.has_key? :device
+        params[:device] = @product['devices'].first unless @product['devices'].nil?
+      end
+
+      raise MissingArgument, 'Param :device is required' unless params.has_key? :device
 
       request "/v1.2/#{vertical}/#{market}/#{asset}/#{product_id}/ranks", params
     end
