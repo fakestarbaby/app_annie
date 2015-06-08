4655f9c454d7442f466626b976c4f36a6de93fcd
Pavel Borsky <pavelb@zeptolab.com>
2015-06-08 15:55:11 +0300

    App ranks request

--- lib/app_annie/app.rb
+++ lib/app_annie/app.rb
@@ -46,12 +46,6 @@ module AppAnnie
       raise MissingArgument, 'Param :start_date is required' unless params.has_key? :start_date
       raise MissingArgument, 'Param :end_date is required' unless params.has_key? :end_date
 
-      unless params.has_key? :device
-        params[:device] = @product['devices'].first unless @product['devices'].nil?
-      end
-
-      raise MissingArgument, 'Param :device is required' unless params.has_key? :device
-
       request "/v1.2/#{vertical}/#{market}/#{asset}/#{product_id}/ranks", params
     end
   end
